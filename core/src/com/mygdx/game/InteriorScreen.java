2b3b36dd6115fcb3dadb4d763392c0fac39b6f34
karsfan <karsfan3001@gmail.com>
2017-05-25 14:54:56 +0200

    Interior1

--- core/src/com/mygdx/game/InteriorScreen.java
+++ core/src/com/mygdx/game/InteriorScreen.java
@@ -1,8 +1,31 @@
 package com.mygdx.game;
 
+import java.util.Iterator;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.src.Character.Character;
+import com.mygdx.game.src.Character.DynamicObjects.State;
+import com.mygdx.game.src.World.Game;
 
-public class InteriorScreen implements Screen {
+public class InteriorScreen extends PlayScreen implements Screen {
+	public InteriorScreen(GameSlagyom game, String name) {
+		super(game, name);
+	}
 	
-
+	
+	
 }
