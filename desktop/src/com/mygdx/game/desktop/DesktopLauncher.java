83a5afb8158552ffb3056d347871f1e3054fdd94
karsfan <karsfan3001@gmail.com>
2017-06-07 17:24:15 +0200

    coin

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,6 @@ import com.mygdx.game.GameSlagyom;
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		// config.resizable = false;
 		new LwjglApplication(new GameSlagyom(), config);
-
 	}
 }
