3231e7653241939e085add55291ef1459a0bf4b6
Nicholas96 <nlaurenzano95@gmail.com>
2017-06-14 15:54:50 +0200

    hide

--- core/src/com/mygdx/game/src/World/GameConfig.java
+++ core/src/com/mygdx/game/src/World/GameConfig.java
@@ -5,11 +5,7 @@ public class GameConfig {
 	public static float WIDTH = 1440;
 	public static float HEIGHT = 960;
 	public static int numEnemy;
-<<<<<<< HEAD
 	public static int numMan = 50;
-=======
-	public static int numMan = 30;
->>>>>>> 2e74702d70de0e56ba4106e2e595d881bb407d36
 	public static int numWoman;
 	public static int numItems = 75;
 	
